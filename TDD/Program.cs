a51de53a6d1f585c88479a63c606d0f695c2b59e
Pitchiu <kocitypu@gmail.com>
2023-03-13 11:38:17 +0100

    First commit

---
+++ TDD/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TDD
+{
+    public class StringCalculator
+    {
+
+        public int Calculate(string arg)
+        {
+            if (arg == "")
+            {
+                return 0;
+            }
+            int result;
+            bool success = int.TryParse(arg, out result);
+            if (success)
+            {
+                return result;
+            }
+
+            char[] delimiters = { ',', '\n' };
+            var eitherSplit = arg.Split(delimiters);
+            int sum = 0;
+            foreach (var item in eitherSplit)
+            {
+                int value = Int32.Parse(item);
+                sum += value;
+            }
+
+            return sum;
+        }
+    }
+
+
+    public class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
---
+++ TDDTests/StringCalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Tests
+{
+    [TestClass()]
+    public class StringCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculatorShouldReturnZeroWithEmptyString()
+        {
+            var calculator = new StringCalculator();
+            var input = "";
+
+            var result = calculator.Calculate(input);
+
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod()]
+        public void CalculatorShouldReturnValueWithSingleNumber()
+        {
+            var calculator = new StringCalculator();
+            var input = "1";
+
+            var result = calculator.Calculate(input);
+
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod()]
+        public void CalculatorShouldReturnSumForCommaSeparatedValues()
+        {
+            var calculator = new StringCalculator();
+            var input = "3,5";
+
+            var result = calculator.Calculate(input);
+
+            Assert.IsTrue(result == 8);
+        }
+
+        [TestMethod()]
+        public void CalculatorShouldReturnSumForNewlineDelimitors()
+        {
+            var calculator = new StringCalculator();
+            var input = "3\n5";
+
+            var result = calculator.Calculate(input);
+
+            Assert.IsTrue(result == 8);
+        }
+
+        [TestMethod()]
+        public void CalculatorShouldReturnSumOfThreeNumbersForEitherDelimitor()
+        {
+            var calculator = new StringCalculator();
+            var input = "3\n5,3";
+
+            var result = calculator.Calculate(input);
+
+            Assert.IsTrue(result == 11);
+        }
+    }
+}
